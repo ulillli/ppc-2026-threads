dc71d1581fd871b8ad4f43816844c6baf0e3b424
Makasimchik <titaevmaks2005@gmail.com>
2026-03-01 21:14:23 +0300

    Ver1

--- tasks/titaev_m_sortirovka_betchera/tests/functional/main.cpp
+++ tasks/titaev_m_sortirovka_betchera/tests/functional/main.cpp
@@ -15,30 +15,32 @@
 
 #include "titaev_m_sortirovka_betchera/common/include/common.hpp"
 #include "titaev_m_sortirovka_betchera/seq/include/ops_seq.hpp"
+
 #include "util/include/func_test_util.hpp"
 #include "util/include/util.hpp"
 
 namespace titaev_m_sortirovka_betchera {
 GTEST_ALLOW_UNINSTANTIATED_PARAMETERIZED_TEST(TitaevBatcherRadixFuncTests);
 
-// ParamType, с которым реально работает BaseRunFuncTests
-using ParamType =
-    std::tuple<std::function<std::shared_ptr<ppc::task::Task<InType, OutType>>(InType)>, std::string, TestType>;
+using ParamType = std::tuple<
+    std::function<std::shared_ptr<ppc::task::Task<InType, OutType>>(InType)>,
+    std::string, TestType>;
 
-class TitaevBatcherRadixFuncTests : public ppc::util::BaseRunFuncTests<InType, OutType, TestType> {
- public:
-  // gtest сюда передаёт TestParamInfo<ParamType>
-  static std::string PrintTestParam(const testing::TestParamInfo<ParamType> &info) {
-    return std::get<1>(info.param);  // имя теста
+class TitaevBatcherRadixFuncTests
+    : public ppc::util::BaseRunFuncTests<InType, OutType, TestType> {
+public:
+  static std::string
+  PrintTestParam(const testing::TestParamInfo<ParamType> &info) {
+    return std::get<1>(info.param);
   }
 
- protected:
+protected:
   InType input_;
 
   void SetUp() override {
-    // GetParam() имеет тип ParamType
+
     ParamType full_param = GetParam();
-    TestType param = std::get<2>(full_param);  // третий элемент — наш TestType
+    TestType param = std::get<2>(full_param);
 
     const int size = std::get<0>(param);
 
@@ -52,39 +54,40 @@ class TitaevBatcherRadixFuncTests : public ppc::util::BaseRunFuncTests<InType, O
   }
 
   bool CheckTestOutputData(OutType &output) final {
-    if (output.size() != input_.size()) {
+    if (output.size() != input_.size())
       return false;
-    }
     for (size_t i = 1; i < output.size(); i++) {
-      if (output[i] < output[i - 1]) {
+      if (output[i] < output[i - 1])
         return false;
-      }
     }
     return true;
   }
 
-  InType GetTestInputData() final {
-    return input_;
-  }
+  InType GetTestInputData() final { return input_; }
 };
 
 namespace {
 
-// Фабрика задачи
 std::shared_ptr<ppc::task::Task<InType, OutType>> MakeSeqTask(InType in) {
   return std::make_shared<TitaevSortirovkaBetcheraSEQ>(in);
 }
 
-// Набор параметров для gtest
-const ParamType kParamSmall{MakeSeqTask, "titaev_m_sortirovka_betchera_seq_size_100", TestType{100, "size_100"}};
+const ParamType kParamSmall{MakeSeqTask,
+                            "titaev_m_sortirovka_betchera_seq_size_100",
+                            TestType{100, "size_100"}};
 
-const ParamType kParamMedium{MakeSeqTask, "titaev_m_sortirovka_betchera_seq_size_500", TestType{500, "size_500"}};
+const ParamType kParamMedium{MakeSeqTask,
+                             "titaev_m_sortirovka_betchera_seq_size_500",
+                             TestType{500, "size_500"}};
 
-const ParamType kParamLarge{MakeSeqTask, "titaev_m_sortirovka_betchera_seq_size_1000", TestType{1000, "size_1000"}};
+const ParamType kParamLarge{MakeSeqTask,
+                            "titaev_m_sortirovka_betchera_seq_size_1000",
+                            TestType{1000, "size_1000"}};
 
 INSTANTIATE_TEST_SUITE_P(FunctionalSortingTests, TitaevBatcherRadixFuncTests,
-                         ::testing::Values(kParamSmall, kParamMedium, kParamLarge),
+                         ::testing::Values(kParamSmall, kParamMedium,
+                                           kParamLarge),
                          TitaevBatcherRadixFuncTests::PrintTestParam);
 
-}  // namespace
-}  // namespace titaev_m_sortirovka_betchera
+} // namespace
+} // namespace titaev_m_sortirovka_betchera
--- tasks/titaev_m_sortirovka_betchera/tests/performance/main.cpp
+++ tasks/titaev_m_sortirovka_betchera/tests/performance/main.cpp
@@ -1,44 +1,55 @@
 #include <gtest/gtest.h>
 
-#include "example_processes/common/include/common.hpp"
-#include "example_processes/mpi/include/ops_mpi.hpp"
-#include "example_processes/seq/include/ops_seq.hpp"
+#include "titaev_m_sortirovka_betchera/common/include/common.hpp"
+#include "titaev_m_sortirovka_betchera/seq/include/ops_seq.hpp"
 #include "util/include/perf_test_util.hpp"
+#include <random>
 
-namespace nesterov_a_test_task_processes {
+namespace titaev_m_sortirovka_betchera {
 
-class ExampleRunPerfTestProcesses : public ppc::util::BaseRunPerfTests<InType, OutType> {
-  const int kCount_ = 100;
-  InType input_data_{};
+class TitaevBatcherRadixPerfTests
+    : public ppc::util::BaseRunPerfTests<InType, OutType> {
+protected:
+  static constexpr size_t kSize = 200000;
+  InType input_;
 
   void SetUp() override {
-    input_data_ = kCount_;
+    std::mt19937 gen(777);
+    std::uniform_real_distribution<double> dist(-10000.0, 10000.0);
+
+    input_.resize(kSize);
+    for (size_t i = 0; i < kSize; i++) {
+      input_[i] = dist(gen);
+    }
   }
 
-  bool CheckTestOutputData(OutType &output_data) final {
-    return input_data_ == output_data;
+  bool CheckTestOutputData(OutType &output) final {
+    for (size_t i = 1; i < output.size(); i++) {
+      if (output[i] < output[i - 1])
+        return false;
+    }
+    return true;
   }
 
-  InType GetTestInputData() final {
-    return input_data_;
-  }
+  InType GetTestInputData() final { return input_; }
 };
 
-TEST_P(ExampleRunPerfTestProcesses, RunPerfModes) {
+TEST_P(TitaevBatcherRadixPerfTests, RunPerformanceModes) {
   ExecuteTest(GetParam());
 }
 
 namespace {
 
-const auto kAllPerfTasks =
-    ppc::util::MakeAllPerfTasks<InType, NesterovATestTaskMPI, NesterovATestTaskSEQ>(PPC_SETTINGS_example_processes);
+const auto kPerfTasks =
+    ppc::util::MakeAllPerfTasks<InType, TitaevSortirovkaBetcheraSEQ>(
+        PPC_SETTINGS_titaev_m_sortirovka_betchera);
 
-const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
+const auto kValues = ppc::util::TupleToGTestValues(kPerfTasks);
 
-const auto kPerfTestName = ExampleRunPerfTestProcesses::CustomPerfTestName;
+const auto kNameGen = TitaevBatcherRadixPerfTests::CustomPerfTestName;
 
-INSTANTIATE_TEST_SUITE_P(RunModeTests, ExampleRunPerfTestProcesses, kGtestValues, kPerfTestName);
+INSTANTIATE_TEST_SUITE_P(PerformanceSortingTests, TitaevBatcherRadixPerfTests,
+                         kValues, kNameGen);
 
-}  // namespace
-
-}  // namespace nesterov_a_test_task_processes
+} // namespace
+} // namespace titaev_m_sortirovka_betchera
